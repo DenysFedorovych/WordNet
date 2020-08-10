a22ea31174bf0350709eaa62f8075f7e8a64e4a7
DenysFedorovych <63046982+DenysFedorovych@users.noreply.github.com>
2020-08-10 23:32:31 +0300

    Constructor ready

--- src/com/company/WordNet.java
+++ src/com/company/WordNet.java
@@ -1,41 +1,71 @@
 package com.company;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.BinarySearchST;
+import java.util.ArrayList;
 
 public class WordNet {
     /// CLASS NODE
     private class Node implements Comparable<Node>
     {
+        private boolean special;
         public int id;
         public String[] strings;
-        public Node(int id, String string)
+        public Node(int id, String string, boolean special)
         {
             this.id = id;
             this.strings = string.split(" ");
+            this.special = special;
         }
 
         @Override
         public int compareTo(Node o) {
-            return 0;
+            if(this.special)
+            {
+                for(String each: o.strings){
+                    if(each.equals(this.strings[0])){return 0;}
+                }
+            }
+            return this.strings[0].compareTo(o.strings[0]);
         }
     }
 
     /// CLASS WORDNET
 
+    private String[] nouns = new String[82191];
+    private BinarySearchST tree;
+    private Digraph graph = new Digraph(82192);
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
     {
         this.checkNull(synsets);
         this.checkNull(hypernyms);
+        tree = new BinarySearchST();
         In sns = new In(synsets);
         In hps = new In(hypernyms);
+        while(hps.hasNextLine())
+        {
+            String[] current = hps.readLine().split(",");
+            for(String each : current){
+                graph.addEdge(Integer.parseInt(current[0]),Integer.parseInt(each));
+            }
+        }
+        while(sns.hasNextLine())
+        {
+            String[] current = sns.readLine().split(",");
+            int id = Integer.parseInt(current[0]);
+            nouns[id] = current[1];
+            for(String each : current[1].split(" ")){tree.put(each,id);}
+        }
 
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns()
     {
-
+        ArrayList<String> array = new ArrayList<>();
+        for(String each : nouns){array.add(each);}
+        return array;
     }
 
     // is the word a WordNet noun?
