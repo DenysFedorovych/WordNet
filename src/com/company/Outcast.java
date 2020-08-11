e4f08a18df1050336bdd697edf396bb664ade35d
DenysFedorovych <63046982+DenysFedorovych@users.noreply.github.com>
2020-08-11 04:22:16 +0300

    first attempt

--- src/com/company/Outcast.java
+++ src/com/company/Outcast.java
@@ -1,9 +1,27 @@
 package com.company;
 
 public class Outcast {
-    public Outcast(WordNet wordnet)         // constructor takes a WordNet object
+    private WordNet wordnet;
+    public Outcast(WordNet wordnet)
+    {
+        this.wordnet = wordnet;
+    }
+    // constructor takes a WordNet object
 
-    public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns)
+    {
+        String result=null;
+        int res = 100000000;
+        for(String each : nouns)
+        {
+            int dist=0;
+            for(String str : nouns)
+            {dist+= wordnet.distance(each,str);}
+            if(dist<res){res=dist; result = each;}
+        }
+        return result;
+    }
+    // given an array of WordNet nouns, return an outcast
 
-    public static void main(String[] args)  // see test client below
+    public static void main(String[] args){}  // see test client below
 }
